e743672016665b79be77abb39c5f69d875bab4f6
Jake Pitman <jakeseanp@gmail.com>
2024-06-30 15:28:01 +0900

    Make active <NavItem/> lower

--- app/_components/NavItem.tsx
+++ app/_components/NavItem.tsx
@@ -1,12 +1,31 @@
+import cx from "classnames";
+
 type NavItemProps = {
   label: string;
   onClick: () => void;
+  isActive: boolean;
+  symbol: string;
 };
 
-export const NavItem = ({ label, onClick }: NavItemProps) => {
+export const NavItem = ({ label, onClick, isActive, symbol }: NavItemProps) => {
   return (
-    <li>
-      <button onClick={onClick}>{label}</button>
+    <li
+      className={cx("px-8 py-4 mr-12 h-min", {
+        "bg-slate-500": !isActive,
+        "bg-slate-900 mt-8": isActive,
+      })}
+    >
+      <button onClick={onClick}>
+        <span
+          className={cx("mr-8 aspect-square w-24 inline-block", {
+            "bg-slate-900": !isActive,
+            "bg-slate-300": isActive,
+          })}
+        >
+          {symbol}
+        </span>
+        {label}
+      </button>
     </li>
   );
 };
--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,17 +1,33 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 import { NavItem } from "@components/NavItem";
 
 export const Navbar = () => {
   const router = useRouter();
+  const pathname = usePathname();
 
   return (
-    <div className="fixed top-0 left-0 w-full">
+    <div className="fixed top-0 left-0 w-full border-solid border-[1px] mt-12 px-12">
       <ul className="flex">
-        <NavItem label="Home" onClick={() => router.push("/")} />
-        <NavItem label="Articles" onClick={() => router.push("/articles")} />
-        <NavItem label="Contact" onClick={() => router.push("/contact")} />
+        <NavItem
+          label="Home"
+          onClick={() => router.push("/")}
+          isActive={pathname === "/"}
+          symbol="⏀"
+        />
+        <NavItem
+          label="Articles"
+          onClick={() => router.push("/articles")}
+          isActive={pathname === "/articles"}
+          symbol="⎅"
+        />
+        <NavItem
+          label="Contact"
+          onClick={() => router.push("/contact")}
+          isActive={pathname === "/contact"}
+          symbol="⏃"
+        />
       </ul>
     </div>
   );
