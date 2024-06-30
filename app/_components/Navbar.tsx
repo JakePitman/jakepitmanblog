0197e6ee7ef0a0ddb67a685cf35a427947cd59f8
Jake Pitman <jakeseanp@gmail.com>
2024-06-30 19:48:17 +0900

    Animate DotsEmbellishment

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -9,28 +9,53 @@ import { useWindowDimensions } from "@hooks/useWindowDimensions";
 import { fadeIn } from "@utils/sharedVariants";
 
 const DotGroup = () => (
-  <div className="relative mx-12">
+  <motion.div variants={fadeIn} className="relative mx-12">
     <div className="absolute right-[-7px] w-[3px] h-[3px] bg-slate-900" />
     <div className="absolute right-[1px] w-[3px] h-[3px] bg-slate-900" />
     <div className="absolute top-[5px] w-[3px] h-[3px] bg-slate-900" />
-  </div>
+  </motion.div>
 );
 
 const DotsEmbellishment = () => {
   const { width } = useWindowDimensions();
   const numberOfDotGroups = width / 20;
   console.log(numberOfDotGroups);
+  const staggerDuration = 0.02;
 
   return (
     <div className="flex justify-center mt-4 w-full">
       <div className="flex">
-        {Array.from({ length: numberOfDotGroups / 2 }).map((_, i) => (
-          <DotGroup key={i} />
-        ))}
+        <motion.div
+          variants={{
+            itemsAppearing: {
+              transition: {
+                staggerChildren: staggerDuration,
+                staggerDirection: -1,
+              },
+            },
+          }}
+          className="flex"
+        >
+          {Array.from({ length: numberOfDotGroups / 2 }).map((_, i) => (
+            <DotGroup key={i} />
+          ))}
+        </motion.div>
         <DotGroup />
-        {Array.from({ length: numberOfDotGroups / 2 }).map((_, i) => (
-          <DotGroup key={i} />
-        ))}
+        <motion.div
+          className="flex"
+          variants={{
+            itemsAppearing: {
+              transition: {
+                staggerChildren: staggerDuration,
+                staggerDirection: 1,
+              },
+            },
+          }}
+        >
+          {Array.from({ length: numberOfDotGroups / 2 }).map((_, i) => (
+            <DotGroup key={i} />
+          ))}
+        </motion.div>
       </div>
     </div>
   );
@@ -91,9 +116,9 @@ export const Navbar = () => {
               />
             </div>
           </motion.div>
-          <motion.div variants={fadeIn}>
-            <IoSettingsOutline size={35} className="text-slate-900 mt-4 mr-4" />
-          </motion.div>
+          <motion.button variants={fadeIn}>
+            <IoSettingsOutline size={35} className="text-slate-900  mr-4" />
+          </motion.button>
         </div>
         <motion.div
           variants={underlineVariants}
