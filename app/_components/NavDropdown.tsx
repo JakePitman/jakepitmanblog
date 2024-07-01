e1cd67c4029d1e39106e863429eedd6ff099f17a
Jake Pitman <jakeseanp@gmail.com>
2024-07-01 12:00:54 +0900

    Add close icon

--- app/_components/NavDropdown.tsx
+++ app/_components/NavDropdown.tsx
@@ -1,4 +1,6 @@
 import cx from "classnames";
+import { IoCloseSharp } from "react-icons/io5";
+
 export const NavDropdown = () => {
   return (
     <div
@@ -11,9 +13,14 @@ export const NavDropdown = () => {
         "left-0 right-0 top-0 bottom-0 m-auto fixed w-11/12"
       )}
     >
-      <div className="bg-slate-900 p-8">
-        <h2 className="text-slate-300 sm:block hidden">Settings</h2>
-        <h2 className="text-slate-300 sm:hidden">Menu</h2>
+      <div className="bg-slate-900 p-8 flex justify-between items-center">
+        <h2 className="text-20 text-slate-300 tracking-wide sm:block hidden">
+          Settings
+        </h2>
+        <h2 className="text-20 text-slate-300 tracking-wide sm:hidden">Menu</h2>
+        <button>
+          <IoCloseSharp className="text-slate-300 text-30 sm:text-24" />
+        </button>
       </div>
       My cool dropdown
     </div>
