65f917483874c0c21f203aa6d6b0ac12fbf37008
Jake Pitman <jakeseanp@gmail.com>
2024-07-03 19:57:33 +0900

    Make image slightly larger

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="relative border-[1px] border-solid h-full ">
-      <div className="w-3/5 aspect-square absolute top-0 right-0 bottom-0 left-0  m-auto">
+      <div className="w-4/6 aspect-square absolute top-0 right-0 bottom-0 left-0  m-auto">
         <div className="relative w-full h-full">
           <Image
             src={"/images/washitsu.jpeg"}
