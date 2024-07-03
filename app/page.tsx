8df88825823d211f86824f43c5a976b658ed7ca9
Jake Pitman <jakeseanp@gmail.com>
2024-07-03 20:33:23 +0900

    Add styles to layout position of card groups

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,41 @@ type CardProps = {
   children: React.ReactNode;
 };
 const Card = ({ border, children }: CardProps) => (
-  <div className={cx("relative bg-slate-400")}>
+  <div className={cx("relative bg-slate-400 h-max w-max")}>
     <div
-      className={cx("absolute w-4 h-full bg-slate-900", {
+      className={cx("absolute w-8 h-full bg-slate-900", {
         "right-0": border === "right",
       })}
     />
-    {children}
+
+    <div
+      className={cx("py-8", {
+        "pr-16 pl-12": border === "right",
+        "pr-12 pl-16": border === "left",
+      })}
+    >
+      {children}
+    </div>
   </div>
 );
 
 export default function Home() {
   return (
-    <div className="relative h-full ">
+    <div className="relative h-full">
+      <div className="flex justify-around border-[1px] h-full">
+        <div className="flex items-start h-full pt-128 z-20">
+          <Card border="left">
+            <p>Hello world</p>
+          </Card>
+        </div>
+        <div />
+        <div className="flex items-end h-full pb-128 z-20">
+          <Card border="right">
+            <p>Hello world</p>
+          </Card>
+        </div>
+      </div>
+
       {/* Image */}
       <div className="w-4/6 aspect-square absolute top-0 right-0 bottom-0 left-0  m-auto max-h-[60vh]">
         <div className="relative w-full h-full">
