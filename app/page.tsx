d9dc4dc38aa85f8620abed1189d4592bf0123b68
Jake Pitman <jakeseanp@gmail.com>
2024-07-03 19:51:57 +0900

    Center image on desktop

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="border-[1px] border-solid h-full">
-      <div className="relative w-128 h-128">
-        <Image
-          src={"/images/washitsu.jpeg"}
-          fill
-          style={{ objectFit: "contain" }}
-          alt={"Washitsu"}
-        />
+    <div className="relative border-[1px] border-solid h-full ">
+      <div className="w-3/5 aspect-square absolute top-0 right-0 bottom-0 left-0  m-auto">
+        <div className="relative w-full h-full">
+          <Image
+            src={"/images/washitsu.jpeg"}
+            fill
+            style={{ objectFit: "contain" }}
+            alt={"Washitsu"}
+          />
+        </div>
       </div>
     </div>
   );
