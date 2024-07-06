cbb8ec7f1e32cff76cd906261a3a33396efe082a
Jake Pitman <jakeseanp@gmail.com>
2024-07-06 19:38:09 +0900

    Style title, description and createdAt

--- app/(routes)/articles/[slug]/page.tsx
+++ app/(routes)/articles/[slug]/page.tsx
@@ -1,6 +1,13 @@
-import { SanityDocument } from "next-sanity";
+import { Bebas_Neue } from "next/font/google";
+import cx from "classnames";
 
 import { client } from "@/sanity/client";
+import { FormattedDate } from "@components/FormattedDate";
+
+const bebasNeue = Bebas_Neue({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 const SLUGS_QUERY = `
 *[
@@ -61,10 +68,16 @@ export default async function Article({ params }: ArticleProps) {
 
   const { _createdAt: createdAt, title, description, mainContent } = article;
   return (
-    <div>
-      <h1 className="text-4xl">{title}</h1>
-      <p>{createdAt}</p>
-      <h3>{description}</h3>
+    <div className="w-full flex justify-center mt-12">
+      <div className="w-11/12 shadow-lg border-1 border-slate-600 p-8">
+        <h1 className={cx("text-24 mb-8", bebasNeue.className)}>{title}</h1>
+        <h3 className="border-l-8 border-slate-800 pl-8 mb-8">{description}</h3>
+        <em className="w-full flex items-center text-12">
+          <hr className="flex-grow text-slate-500 mr-4" />
+          <FormattedDate value={createdAt} />
+          <hr className="flex-grow text-slate-500 ml-4" />
+        </em>
+      </div>
     </div>
   );
 }
