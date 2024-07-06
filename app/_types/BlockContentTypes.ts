82d47b98f8bdc6150eceabad08a8437247e75063
Jake Pitman <jakeseanp@gmail.com>
2024-07-07 07:50:12 +0900

    Add types dec for article

---
+++ app/_types/BlockContentTypes.ts
@@ -0,0 +1,28 @@
+type BlockBase = {
+  _type: "block";
+  style: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "blockquote" | "normal";
+  children: {
+    text: string | null; // TODO: Check if empty lines are null or ""
+    marks: ("strong" | "em" | "underline" | "strikethrough" | "code")[];
+  }[];
+};
+
+type WithoutSpecialFields = {
+  listItem: never;
+  level: never;
+};
+type WithListItem = {
+  listItem: "bullet" | "number";
+  level: number;
+};
+
+// Ensure that listItem always comes with level and vice versa
+export type Block = BlockBase & (WithoutSpecialFields | WithListItem);
+
+export type Code = {
+  _type: "code";
+  code: string;
+  language: "typescript";
+};
+
+export type BlockContent = Block | Code;
