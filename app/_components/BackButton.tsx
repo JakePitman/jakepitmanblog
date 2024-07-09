55fa1d1748cd553c4186859f253677dece53acf7
Jake Pitman <jakeseanp@gmail.com>
2024-07-10 00:39:09 +0900

    Animate back button

--- app/_components/BackButton.tsx
+++ app/_components/BackButton.tsx
@@ -1,14 +1,20 @@
 "use client";
 import { BsBoxArrowLeft } from "react-icons/bs";
 import { FormattedMessage } from "react-intl";
+import { motion } from "framer-motion";
 import Link from "next/link";
 
 export const BackButton = () => (
-  <Link
-    href="/articles"
-    className="lg:fixed lg:left-12 mt-8 flex items-center text-18 text-slate-900 z-20"
+  <motion.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1, transition: { duration: 0.6 } }}
   >
-    <BsBoxArrowLeft className="mr-8" />
-    <FormattedMessage id="article.backButton" defaultMessage="Back" />
-  </Link>
+    <Link
+      href="/articles"
+      className="lg:fixed lg:left-12 mt-8 flex items-center text-18 text-slate-900 z-20"
+    >
+      <BsBoxArrowLeft className="mr-8" />
+      <FormattedMessage id="article.backButton" defaultMessage="Back" />
+    </Link>
+  </motion.div>
 );
